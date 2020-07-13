67c75b13d2fefa68e62a2f164c65fa3f7b3be0f5
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-07-13 20:50:10 -0300

    importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import profile from './assets/profile.png'
+
 function App(){
-  return <h1>Hello Marcelo vamos codar até conseguir uma vaga.</h1>
+  return <div>
+    <h1>Hello Marcelo vamos codar até conseguir uma vaga.</h1>
+    <img src={profile} alt="Marcelo Rafael Gonçalves"/>
+  </div>
 }
 
 export default App;
