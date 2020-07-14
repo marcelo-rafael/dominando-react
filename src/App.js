312498e1e877785690c438c6e0ec0cd913e7a3e0
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-07-13 21:06:33 -0300

    class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/profile.png'
+import TechList from './components/TechList';
 
 function App(){
   return <div>
-    <h1>Hello Marcelo vamos codar até conseguir uma vaga.</h1>
-    <img src={profile} alt="Marcelo Rafael Gonçalves"/>
+    <TechList />
   </div>
 }
 
